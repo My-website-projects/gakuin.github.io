f62728c06beda74b4b20aeb12d385fabf92b2d75
Pamod Bulathgama <64356305+pamod99@users.noreply.github.com>
2023-06-05 07:50:57 +0530

    update map and others

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 // Scroll to the top of the page
 function scrollToTop() {
     window.scrollTo(0, document.body.scrollHeight);
@@ -15,20 +16,4 @@ function scrollToTop() {
   // Call the refreshAfterScrollWithDelay function with a delay of 2000 milliseconds (2 seconds)
   refreshAfterScrollWithDelay(2000);
 
-  // Function to clear cache and refresh the page
-function clearCacheAndRefresh() {
-  if (caches) {
-    // Clear the cache
-    caches.keys().then(function(names) {
-      for (let name of names) {
-        caches.delete(name);
-      }
-    });
-  }
 
-  // Refresh the page
-  location.reload();
-}
-
-// Call the clearCacheAndRefresh function when needed
-clearCacheAndRefresh();
